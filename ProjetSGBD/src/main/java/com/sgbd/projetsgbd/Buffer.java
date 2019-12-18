0bdda6ba790b4e22e40ff4a5cb3a4d025edd0f09
af433882 <arnaud.fr21@gmail.com>
2019-12-18 14:27:27 +0100

    buffer classe

--- ProjetSGBD/src/main/java/com/sgbd/projetsgbd/Buffer.java
+++ ProjetSGBD/src/main/java/com/sgbd/projetsgbd/Buffer.java
@@ -5,10 +5,36 @@
  */
 package com.sgbd.projetsgbd;
 
+import java.util.List;
+
 /**
  *
  * @author tb985403
  */
 public class Buffer {
+    private int capacite;
+    private List<Bloc> b;
+
+    public Buffer(int capacite, List<Bloc> b) {
+        this.capacite = capacite;
+        this.b = b;
+    }
+
+    public int getCapacite() {
+        return capacite;
+    }
+
+    public void setCapacite(int capacite) {
+        this.capacite = capacite;
+    }
+
+    public List<Bloc> getB() {
+        return b;
+    }
+
+    public void setB(List<Bloc> b) {
+        this.b = b;
+    }
+    
     
 }
